5cdb63bd2096bc243931b84099ce5a6f7b07fc89
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-06 21:08:03 +0000

    creating routes

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 
+
 // set up express app
 const app = express();
 
-app.get("/api", function(req, res){
-  res.send({name: "Yoshi"});
-});
+
+app.use("/api", require("./routes/api"));
+// initialize routes
+
 
 // listen for requests
 app.listen(process.env.PORT || process.env.IP, function(){
 	console.log('now listening for requests');
 });
 
-// app.get('/api', function(req, res){
-//   res.send({ name: 'Yoshi' });
-// });
+
+// // initialize routes
+// app.use('/api', require('./routes/api'));
