696231eae515c2a653384b7434e67aa4af2e2f45
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-06 20:01:54 +0000

    Handling Requests

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const express = require('express');
 // set up express app
 const app = express();
 
+app.get("/api", function(req, res){
+  res.send({name: "Yoshi"});
+});
+
+// listen for requests
 app.listen(process.env.PORT || process.env.IP, function(){
 	console.log('now listening for requests');
 });
 
-
-
-
-// const express = require('express');
-
-// // set up express app
-// const app = express();
-// // listen for requests
-// app.listen(process.env.port || 4000, function(){
-//    console.log('now listening for requests');
+// app.get('/api', function(req, res){
+//   res.send({ name: 'Yoshi' });
 // });
