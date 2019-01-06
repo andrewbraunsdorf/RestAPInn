22e823a9b1c607d48cb6afea53135ccf654d6733
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-06 22:06:37 +0000

    created PUT and DELETE requests using Postman.

--- routes/api.js
+++ routes/api.js
@@ -12,17 +12,16 @@ router.post("/ninjas", function(req, res){
 });
 
 //update a ninja in the db
-router.put("/ninjas/id", function(req, res){
+router.put("/ninjas/:id", function(req, res){
     res.send({type: "PUT"});
 });
 
 //delete a ninja from the db
-router.delete("/ninjas/id", function(req, res){
+router.delete("/ninjas/:id", function(req, res){
     res.send({type: "DELETE"});
 });
 
 module.exports = router;
-
 
 
 // const express = require ('express');
