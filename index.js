f3de8a3d720cb2af305d4d194f13afa8896780f5
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-09 02:26:02 +0000

    creating a react component

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose =require("mongoose");
+const mongoose = require('mongoose');
 
 // set up express app
 const app = express();
 
-
-//connect to mongodb
-mongoose.connect("mongodb://localhost/ninjago", { useNewUrlParser: true });
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
 mongoose.Promise = global.Promise;
 
-app.use(express.static("public"));
+//set up static files
+app.use(express.static('public'));
 
 // use body-parser middleware
 app.use(bodyParser.json());
@@ -18,13 +18,14 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api', require('./routes/api'));
 
-//error handling middleware
+// error handling middleware
 app.use(function(err, req, res, next){
-	//console.log(err);
-	res.status(422).send({error: err.message});
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
 });
 
 // listen for requests
 app.listen(process.env.PORT || process.env.IP, function(){
 	console.log('now listening for requests');
 });
+
--- ninja.js
+++ ninja.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+});
+
 // create ninja Schema & model
 const NinjaSchema = new Schema({
     name: {
@@ -9,12 +21,12 @@ const NinjaSchema = new Schema({
     },
     rank: {
         type: String
-    }
+    },
     available: {
         type: Boolean,
         default: false
-    }
-    // add in geo location
+    },
+    geometry: GeoSchema
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
